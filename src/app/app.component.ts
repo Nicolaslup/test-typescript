22a2077ca14df7d5940a162721cbd8cc92683269
Nicolaslup <nicolas.lupieri@gmail.com>
2019-07-05 00:26:59 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+export class Test {
+  name: string;
+  age: number;
+
+  constructor() {
+    this.name = '';
+    this.age = 0;
+  }
+}
+
+export function sortTyping<T>(DynamicClass: new (...params: any[]) => T, tab: Array<T>, key: string): Array<T> | null {
+  const obj = Object.getOwnPropertyNames(new DynamicClass());
+  
+  let newTab: Array<T> | null = new Array<T>();
+  if (obj.includes(key)) {
+    newTab = tab.sort((a: T, b: T) => {
+        return a[key] - b[key];
+      }
+    )
+  } else {
+    newTab = null;
+  }
+  return newTab
+}
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent implements OnInit {
+
+  public tab2: Array<Test>;
+  ngOnInit():void {
+    const tab: Array<Test> = new Array<Test>(
+      {name: 'john', age: 18},
+      {name: 'celia', age: 20},
+      {name: 'tom', age: 16},
+      {name: 'nic', age: 21},
+    );
+
+    this.tab2 = sortTyping<Test>(Test, tab, 'age')
+  }
+  
 }
+
